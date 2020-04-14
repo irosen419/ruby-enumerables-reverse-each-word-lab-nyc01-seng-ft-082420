96f2732d0a4dd6f2fe64b92bfea7560d09feff22
Ian Rosen <irosen419@gmail.com>
2020-04-14 17:51:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,12 @@
+require 'pry'
 def reverse_each_word(string)
   
   array = string.split
+  binding.pry
   final_array = array.collect do |index|
     index.reverse
   end
   final_array.join(" ")
 end
+
+reverse_each_word("Hello there.")
